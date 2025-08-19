eaf874d5697178b7d07ea4512fc3c8c0ea1a398d
BigDragonlin <51269337+BigDragonlin@users.noreply.github.com>
2025-08-19 10:18:52 +0800

    更新 VSCode 配置，将 Java 构建配置更新模式修改为自动，并在 pom.xml 中新增测试依赖和 Maven Surefire 插件，以支持单元测试功能。

---
+++ ruoyi-admin/src/test/java/com/ruoyi/web/controller/common/CaptchaControllerTest.java
@@ -0,0 +1,12 @@
+package com.ruoyi.web.controller.common;
+
+import org.junit.jupiter.api.Test;
+
+class CaptchaControllerTest {
+    @Test
+    void testHelloWorld() {
+        System.out.println("Hello World _________________");
+        // 打印
+        System.out.println("Hello World _________________dfasdfasd");
+    }
+}
